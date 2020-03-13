146ee48fd659434f3f52a6fbdda3601d130f4a36
chen <1441088980@qq.com>
2020-03-14 00:04:04 +0800

    日常提交

---
+++ algorithms/多数元素.java
@@ -0,0 +1,66 @@
+/**
+ * Created by handsome programmer.
+ * User: chen
+ * Date: 2020/3/13
+ * Time: 19:09
+ * Description:
+ *
+ * @author chen
+ */
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int length = nums.length;
+        quickSort(nums, 0, length - 1);
+        int result = nums[length >> 1];
+        return result;
+    }
+
+    /**
+     * 快速排序
+     *
+     * @param array 待排序数组
+     * @param left  左边界
+     * @param right 右边界
+     */
+    public static void quickSort(int[] array, int left, int right) {
+        if (left > right) {
+            return;
+        }
+        //设定基准数
+        int temp = array[left];
+        //设置哨兵
+        int i = left;
+        int j = right;
+        while (i != j) {
+            //顺序很重要，要想从右往左找
+            while (array[j] >= temp && i < j) {
+                j--;
+            }
+            //再从左往右找
+            while (array[i] <= temp && i < j) {
+                i++;
+            }
+            //交换两个哨兵在数组中的位置
+            //两个哨兵没有相遇
+            if (i < j) {
+                int t = array[i];
+                array[i] = array[j];
+                array[j] = t;
+            }
+        }
+        //最终将基准数归位
+        array[left] = array[i];
+        array[i] = temp;
+        //继续处理左边的，这是一个递归的过程
+        quickSort(array, left, i - 1);
+        //继续处理右边的，这是一个递归的过程
+        quickSort(array, i + 1, right);
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 2, 1, 1, 1, 2, 2};
+        Solution s = new Solution();
+        int majorityElement = s.majorityElement(array);
+        System.out.println(majorityElement);
+    }
+}
--- algorithms/字符串的最大公因子.java
+++ algorithms/字符串的最大公因子.java
@@ -9,4 +9,4 @@ class Solution {
     private int gcd(int a, int b) {
         return b == 0? a: gcd(b, a % b);
     }
-}
+} 
