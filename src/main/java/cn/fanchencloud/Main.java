184988604631420cf9eb4672f4b9f3872a57a1d0
chen <fanchencloud@qq.com>
2023-10-12 21:39:30 +0800

    import maven

---
+++ src/main/java/cn/fanchencloud/Main.java
@@ -0,0 +1,14 @@
+package cn.fanchencloud;
+
+/**
+ * Version :   V1.0  <br/>
+ * Datetime:   2023/10/12 21:35   <br/>
+ * Description: ${DESC}
+ *
+ * @author: chen
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
